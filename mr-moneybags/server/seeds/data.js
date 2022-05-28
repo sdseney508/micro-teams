e1f8b363e36373b48667cdc85a61e236a5a8f4b5
Steve "Homeschool" Seney <sdseney508@gmail.com>
2022-05-28 09:58:47 -0400

    updated server

---
+++ mr-moneybags/server/seeds/data.js
@@ -0,0 +1,85 @@
+const { getMaxListeners } = require("../models/Portfolios");
+
+const emails = [
+  "sdseney@hotmail.com",
+  "sdseney508@gmail.com",
+  "test_email3@test.com",
+  "test_email4@test.com",
+  "test_email5@test.com",
+  "test_email6@test.com",
+  "test_email7@test.com",
+  "test_email8@test.com",
+  "test_email9@test.com",
+  "test_email10@test.com",
+];
+
+const portfolios = [
+  {
+    portfolioName: "Port 1",
+    stocks: [
+      {
+        name: "IBM",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+      {
+        name: "F",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+      {
+        name: "AAPL",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+    ],
+  },
+  {
+    portfolioName: "Port 2",
+    stocks: [
+      {
+        name: "GOOGL",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+      {
+        name: "SONY",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+      {
+        name: "TWTR",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+    ],
+  },
+  {
+    portfolioName: "Port 3",
+    stocks: [
+      {
+        name: "TSLA",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+      {
+        name: "GM",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+      {
+        name: "GE",
+        dateAdded: Date.now(),
+        purchasePrice: 10,
+      },
+    ],
+  },
+];
+
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const getEmail = () => `${getRandomArrItem(emails)}`;
+
+const makeportfolio = () => getRandomArrItem(portfolios);
+
+module.exports = { makeportfolio, getEmail };
